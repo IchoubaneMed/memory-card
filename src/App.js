dbff3d72a8729b77398ae38a280b561e1db1eb07
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-10-22 13:36:01 +0200

    Added logic to the App.js file

--- src/App.js
+++ src/App.js
@@ -3,40 +3,59 @@ import './App.css'
 import data from './data'
 
 function App() {
-  const dataArr = data.slice(0, 4);
-  const [cars, setCars] = useState(() => dataArr.sort(() => Math.random() - 0.5));
+  let dataArr = data;
+  const [cars, setCars] = useState(() => dataArr.slice(0, 4).sort(() => Math.random() - 0.5));
 
   const [clickedCard, setClickedCard] = useState([]);
   const [lost, setLost] = useState(false);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   const clicked = (id) => {
     setCars([...cars].sort(() => Math.random() - 0.5));
     if (clickedCard.indexOf(id) === -1) {
+
       setClickedCard(pre => pre.concat(id));
+
+      setScore(pre => pre + 1);
+      
+      setLost(false);
     } else {
       setLost(true);
     }
+    console.log(id)
     console.log(clickedCard);
   }
 
-  console.log(dataArr);
+  
+
 
   useEffect(() => {
     if (lost === true) {
-      setCars(dataArr.slice(0, 2));
-      setLost(false);
+      setCars(dataArr.slice(0, 4).sort(() => Math.random() - 0.5));
+      setScore(0);
+      setClickedCard([]);
     }
-  }, [lost, dataArr]);
+
+    if(score >= 4 && score % 4 === 0 && score < 16) {
+      setCars(dataArr.slice(0, score + 4).sort(() => Math.random() - 0.5))
+    }
+
+    if(score > bestScore) {
+      setBestScore(pre => pre + 1);
+    }
+  }, [lost, score, bestScore, dataArr]);
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{score} || {bestScore}</h1>
+      <h1>{lost.toString()}</h1>
       <div className="card-container">
         {cars.map(item => {
           return (
             <div className="card" key={item.id} onClick={() => clicked(item.id)}>
               <img src={item.src} alt={item.title} />
-              <h1>{item.title}</h1>
+              <h1>{item.id}</h1>
             </div>
           )
         })}
