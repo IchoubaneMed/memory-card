8bb72ade98aa60760d75bc9eaf9c11e12af1dbeb
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-10-22 22:03:40 +0200

    Created data.js file

---
+++ src/data.js
@@ -0,0 +1,35 @@
+import beetle_1 from "./assets/beetle_1.jpg";
+import beetle_2 from "./assets/beetle_2.jpg";
+import beetle_3 from "./assets/beetle_3.jpg";
+import beetle_4 from "./assets/beetle_4.jpg";
+import beetle_5 from "./assets/beetle_5.jpg";
+
+const data = [
+    {
+        id: "beetle_1",
+        src: beetle_1,
+        title: "beetle",
+    },
+    {
+        id: "beetle_2",
+        src: beetle_2,
+        title: "beetle",
+    },
+    {
+        id: "beetle_3",
+        src: beetle_3,
+        title: "beetle",
+    },
+    {
+        id: "beetle_4",
+        src: beetle_4,
+        title: "beetle",
+    },
+    {
+        id: "beetle_5",
+        src: beetle_5,
+        title: "beetle",
+    },
+];
+
+export default data;
