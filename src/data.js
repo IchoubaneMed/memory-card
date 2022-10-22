dfaf925481638c7ee3bbb72334a4d8d8f94e503b
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-10-22 12:22:17 +0200

    Added images to the dataset in the data.js file

--- src/data.js
+++ src/data.js
@@ -3,6 +3,18 @@ import beetle_2 from "./assets/beetle_2.jpg";
 import beetle_3 from "./assets/beetle_3.jpg";
 import beetle_4 from "./assets/beetle_4.jpg";
 import beetle_5 from "./assets/beetle_5.jpg";
+import beetle_6 from "./assets/beetle_6.jpg";
+import beetle_7 from "./assets/beetle_7.jpg";
+import beetle_8 from "./assets/beetle_8.jpg";
+import beetle_9 from "./assets/beetle_9.jpg";
+import citreon_1 from "./assets/citreon_1.jpg";
+import lamborghini_1 from "./assets/lamborghini_1.jpg";
+import lamborghini_2 from "./assets/lamborghini_2.jpg";
+import mercedes_1 from "./assets/mercedes_1.jpg";
+import musting_1 from "./assets/musting_1.jpg";
+import musting_2 from "./assets/musting_2.jpg";
+import musting_3 from "./assets/musting_3.jpg";
+
 
 const data = [
     {
@@ -29,6 +41,61 @@ const data = [
         id: "beetle_5",
         src: beetle_5,
         title: "beetle",
+    },
+    {
+        id: "beetle_6",
+        src: beetle_6,
+        title: "beetle",
+    },
+    {
+        id: "beetle_7",
+        src: beetle_7,
+        title: "beetle",
+    },
+    {
+        id: "beetle_8",
+        src: beetle_8,
+        title: "beetle",
+    },
+    {
+        id: "beetle_9",
+        src: beetle_9,
+        title: "beetle",
+    },
+    {
+        id: "citreon_1",
+        src: citreon_1,
+        title: "citreon",
+    },
+    {
+        id: "lamborghini_1",
+        src: lamborghini_1,
+        title: "lamborghini",
+    },
+    {
+        id: "lamborghini_2",
+        src: lamborghini_2,
+        title: "lamborghini",
+    },
+    {
+        id: "mercedes_1",
+        src: mercedes_1,
+        title: "mercedes",
+    },
+    {
+        id: "musting_1",
+        src: musting_1,
+        title: "musting",
+    },
+    {
+        id: "musting_2",
+        src: musting_2,
+        title: "musting",
+    },
+    {
+        id: "musting_3",
+        src: musting_3,
+        title: "musting",
     },
 ];
 
