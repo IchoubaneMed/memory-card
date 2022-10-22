b4ed3f8162daedb89f87a943812b7b2212aeaa26
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-10-22 17:15:18 +0200

    Added Header function to the Header.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
-const Header = () => {
+import './Header.css'
+
+const Header = (props) => {
+    const {score, bestScore} = props;
     return(
-        <></>
+        <div className="container">
+            <h1>Memory card</h1>
+            <div className="score">
+                <h2>Score: {score}</h2> 
+                <h2>Best score: {bestScore}</h2>
+            </div>
+        </div>
     );
 }
 
